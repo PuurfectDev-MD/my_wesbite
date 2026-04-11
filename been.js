99138e2e941c403bb2d354e2ee83b50cb8221ef6
PuurfectDev-MD <new.manishd@gmail.com>
2026-04-11 10:16:22 +0545

    Project update

--- been.js
+++ been.js
@@ -18,7 +18,7 @@ export const beenInfo = [
         description: "Internation Level Robotics compeititon for ages 15 and above",
         comments: "This was like coughing baby vs hydrogen bomb (pls get the reference 🙏🏼) compared to the well-experienced and guided people from college by their mentors. We had no idea of what we were doing with the mechanisms. It was just good vibes and a lot of stress. Got 10th.",
         location: "UAE, Abhu Dhabi",
-        date:"2023",
+        date: "2023",
         photos: ["/assets/images/been/makex.webp", "y"],
         video: null
     },
@@ -26,18 +26,18 @@ export const beenInfo = [
         connecter: "at",
         title: "TECHTOS Game Developement Competition 2024",
         description: "Game development compeition held in my school (Oxford School,Dubai)",
-        comments:"Got all gold medals (total 3!!!) for all the competiton - game/speech/ar and vr experience devlopment",
+        comments: "Got all gold medals (total 3!!!) for all the competiton - game/speech/ar and vr experience devlopment",
         location: "UAE, Dubai",
         date: "2024",
         photos: ["/assets/images/been/Techtos.webp"],
         video: null,
         resources: ["https://drive.google.com/file/d/1nJemglsXH5-fLXxxavT8RwRKPnB2NSnw/view?usp=drive_link"]
     },
-       {
+    {
         connecter: "at",
         title: "JSS International School TechConnect 2.0 Interschool Coding Competition",
         description: "Interschool coding compeition (For HTML) at JSS International School",
-        comments:"i swear this was rigged",
+        comments: "i swear this was rigged",
         location: "UAE, Dubai",
         date: "2024",
         photos: ["x", "y"],
@@ -47,7 +47,7 @@ export const beenInfo = [
         connecter: "at",
         title: "Gulf Medical University Tech Exhibition",
         description: "Exibition for schools all over the UAE",
-        comments:"We made a autonomous pharmaceutical delivery robot. Had a pretty good prototype and got recognition too. But had to leave early from the event cause school didnt let us :( ",
+        comments: "We made a autonomous pharmaceutical delivery robot. Had a pretty good prototype and got recognition too. But had to leave early from the event cause school didnt let us :( ",
         location: "UAE, Dubai",
         date: "2025",
         photos: ["/assets/images/been/Gmu1.webp", "/assets/images/been/Gmu2.webp"],
@@ -55,11 +55,11 @@ export const beenInfo = [
 
     },
 
-     {
+    {
         connecter: "in",
         title: "NASA SPACE APP Challenge",
         description: "App/Web dev challenge for people all around the globe",
-        comments:"I was just bored so tried it with 0 app building experience. It was just all so messed up. No plan no proper team. No expertise. Just got me to get back to web dev after that pathetic attempt 😭. Got out the first round 💔 ",
+        comments: "I was just bored so tried it with 0 app building experience. It was just all so messed up. No plan no proper team. No expertise. Just got me to get back to web dev after that pathetic attempt 😭. Got out the first round 💔 ",
         location: "Virtual",
         date: "2024",
         photos: ["/assets/images/been/nasa_1.webp"],
@@ -69,12 +69,12 @@ export const beenInfo = [
     },
     {
         connecter: "in",
-        title: "Blueprint x AMD",
-        description: "HackClub's teenage funding program for projects",
-        comments:"This was super fun. Got me into kicad and fusion 360. Blueprint really helped me get back in track of being productive and doing something meaningful.",
+        title: "HackClub",
+        description: "A non profit organisation for teenagers to maker!",
+        comments: "Learning about hackclub was the best thing that happened to me for a long time. Making projects was super fun. Got me into kicad,fusion 360, soldering and hands on making  :) ",
         location: "Virtual",
         date: "2026",
-        photos: ["/assets/images/been/blueprint_img1.webp", "/assets/images/been/blueprint_img2.webp"],
+        photos: ["/assets/images/been/fallout.png", "/assets/images/been/stasis.png"],
         video: null
 
     }
--- project.js
+++ project.js
@@ -1,8 +1,16 @@
 // projects info
 
-export const projects =[
+export const projects = [
     {
-        title:"Pawpad",
+        title: "Maker_Base",
+        description: "A knowledge base for makers and developers for documenting their process and concepts.",
+        link: "https://github.com/PuurfectDev-MD/Maker_Base",
+        tags: ["maker base", "note", "documenting", "maker", "svelkit", "sveltekit 5"],
+        image: "/assets/images/3dCad.png",
+        status: "completed"
+    },
+    {
+        title: "Pawpad",
         description: "A cool looking paw shaped macropad for cat people",
         link: "https://github.com/PuurfectDev-MD/Purrfect-Pad",
         tags: ["pawpad", "macropad", "cat", "electronics", "pcb"],
@@ -14,33 +22,47 @@ export const projects =[
         description: "A python project which converts any image into cat noises -can both decode and encode cat noises",
         link: "https://github.com/PuurfectDev-MD/meow-meow",
         tags: ["meow meow", "cat", "python", "image"],
-            status: "completed"
+        status: "completed"
+    },
+    {
+        title: "SvelteKit Blog Website",
+        description: "A private place to write down your thoughts and store! ",
+        link: "https://github.com/PuurfectDev-MD/sveltekit_blog_app",
+        tags: ["blog", "journal", "private blog", "svelkit", "sveltekit 5"],
+        status: "completed"
+    },
+    {
+        title: "Life in pocket",
+        description: "A device to journal your life through pictures, videos and voice memos and upload to a website. ",
+        link: "https://github.com/PuurfectDev-MD/sveltekit_blog_app",
+        tags: ["life in pocket", "custom pcb", "esp32", "journal", "camera"],
+        status: "completed"
     },
     {
         title: "Near365",
         description: "A music player as a gift",
         link: "https://github.com/PuurfectDev-MD/Near365",
         tags: ["gift", "custom esp", "sound", "music player"],
-            status: "completed"
+        status: "completed"
     },
     {
         title: "This very website",
         description: "Personal website to reflect me and store my wacky-cool-creative projects whatver they may be",
         link: "https://github.com/PuurfectDev-MD/landfill",
         tags: ["javascript", "fun", "simple", "website"],
-            status: "completed"
-    },{
+        status: "completed"
+    }, {
         title: "Esp32 Personal assistant",
         description: "Productivity device with Ai api to ask queries and vision to see you",
-        link:"https://github.com/PuurfectDev-MD/esp32-personal-assistant",
-        tags:["esp32", "AI", "productivity", "vision", "micropython"],
-            status: "completed"
+        link: "https://github.com/PuurfectDev-MD/esp32-personal-assistant",
+        tags: ["esp32", "AI", "productivity", "vision", "micropython"],
+        status: "completed"
     },
     {
-        title:"Flask Chat app",
-        description:"My bad attempt at making a chat app back when i was 15",
-        link:"https://github.com/PuurfectDev-MD/flask-chat-web-app",
-        tags:["chat app", "flask", "python"],
-            status: "completed"
+        title: "Flask Chat app",
+        description: "My bad attempt at making a chat app back when i was 15",
+        link: "https://github.com/PuurfectDev-MD/flask-chat-web-app",
+        tags: ["chat app", "flask", "python"],
+        status: "completed"
     }
 ]
